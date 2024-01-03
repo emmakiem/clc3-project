d8f6b758bd52c063ce210dd06dad55df6283b1eb
emmakiem <79919740+emmakiem@users.noreply.github.com>
2024-01-03 16:17:30 +0100

    Update CalculatorApplicationTests.java

--- src/test/java/at/clc/calculator/CalculatorApplicationTests.java
+++ src/test/java/at/clc/calculator/CalculatorApplicationTests.java
@@ -2,12 +2,18 @@ package at.clc.calculator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import at.clc.calculator.services.CalculatorService;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CalculatorApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+    @Test
+    public void testAdd(){
+        assertEquals(4, CalculatorService.add(2,2));
     }
 
 }
