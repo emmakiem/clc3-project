d5c4144bb30fcc9f9985b2903d1490b395b17028
emmakiem <79919740+emmakiem@users.noreply.github.com>
2024-01-03 16:07:36 +0100

    Update CalculatorApplication.java

--- src/main/java/at/clc/calculator/CalculatorApplication.java
+++ src/main/java/at/clc/calculator/CalculatorApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorApplication {
     public static void main(String[] args) {
+        // run application
         SpringApplication.run(CalculatorApplication.class, args);
     }
 }
